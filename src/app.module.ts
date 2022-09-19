f35fc9873144e6e887cb32d68753ce6ea7ae7d04
rey-monstar <rey.baay@monstar-lab.com>
2022-09-19 19:44:49 +0800

    add login feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import {ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
@@ -23,7 +25,9 @@ dotenv.config();
             database: configService.get('DB_NAME'),
         }
       }
-    })
+    }),
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/users/users.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { users } from './users.entity';
+
+// This should be a real class/interface representing a user entity
+export type User = any;
+
+@Injectable()
+export class UsersService {
+
+  constructor(@InjectRepository(users) private userService: Repository<users>){}
+  
+
+  async findOne(email: string, password: string): Promise<User | undefined> {
+    const user = await this.userService.createQueryBuilder().where('email ='+ email).where('password = '+ password).getRawOne();
+    console.log(user, ' check users');
+    return user;
+  }
+}
