17b7a7c3e64f9ef1a2441036c238387f8850ad13
rey-monstar <rey.baay@monstar-lab.com>
2022-09-20 18:33:23 +0800

    Add hashing of password both in registration and login

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './users.entity';
+import * as bcrypt from 'bcrypt'
 
 // This should be a real class/interface representing a user entity
 export type user = any;
@@ -13,15 +14,19 @@ export class UsersService {
   
 
   async findOne(email: string, password: string): Promise<user | undefined> {
+
     const user = await this.userService.createQueryBuilder('users')
     .where(":email = email", {email})
-    .andWhere(':password = password', {password} )
     .getRawOne();
-    return user;
+    const bcryptCompare = await bcrypt.compare(password, user.users_password);
+    if (user && bcryptCompare) {
+      return user;
+    }
+    return null;
   }
 
   async signupUser(users: any): Promise<void> {
-    
+
     const user = new User();
     user.first_name = users.first_name;
     user.last_name = users.last_name;
